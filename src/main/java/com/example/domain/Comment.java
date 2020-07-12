4815d8be8f07d07f466fc7b0186f0e76e0e755bb
shono-ishibashi <shono.ishibashi@rakus-partners.co.jp>
2020-07-12 18:01:20 +0900

    controllerのテストの依存関係の追加

--- src/main/java/com/example/domain/Comment.java
+++ src/main/java/com/example/domain/Comment.java
@@ -1,44 +1,11 @@
 package com.example.domain;
 
+import lombok.Data;
+
+@Data
 public class Comment {
     private String id;
     private String name;
     private String content;
     private String articleId;
-
-    //getter setter
-
-
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public String getArticleId() {
-        return articleId;
-    }
-
-    public void setArticleId(String articleId) {
-        this.articleId = articleId;
-    }
 }
